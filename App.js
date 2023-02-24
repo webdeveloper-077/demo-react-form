89e4743e9f843de04483452e9a51c529f2311d9b
webdeveloper-077 <121479148+webdeveloper-077@users.noreply.github.com>
2023-02-24 09:28:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Login } from "./Login";
+import { Register } from "./Register";
+import './App.css';
+
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
+  );
+}
+
+export default App;
